a0c4bfe1c3a517acf4b6d6d69e5840ab9fa0d135
Dmitrii Shapievskii <dmitrii.shapievskii.cons@koerber.com>
2022-06-07 18:16:41 +0600

    [+] QueryBuilderSimple project

---
+++ PatternExamples/QueryBuiderSimple/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Linq.Expressions;
+using System.Reflection;
+using QueryBuilderSimple;
+
+var builder = new QueryBuilder<Persons>();
+
+string query = builder
+    .From<Persons>()
+    .Select(p => p.Id)
+    .Select(p => p.LastName)
+    .OrderBy(p => p.BirthDate)
+    .Build(true);
+
+builder = new QueryBuilder<Persons>();
+builder
+    .From<Persons>()
+    .OrderBy(p => p.BirthDate)
+    .Build(true);
+
+builder = new QueryBuilder<Persons>();
+builder
+    .From<Persons>()
+    .Build(true);
+
+Console.ReadKey();
---
+++ PatternExamples/QueryBuiderSimple/QueryBuilder.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryBuilderSimple
+{
+    public class QueryBuilder<TTable> : IQueryBuilder, IQueryBuilderSelect<TTable>
+    {
+        private const string TableKey = nameof(TableKey);
+        private const string SelectorsKey = nameof(SelectorsKey);
+        private const string OrderKey = nameof(OrderKey);
+
+        private readonly Dictionary<string, object> _buildContext;
+
+        public QueryBuilder()
+        {
+            _buildContext = new()
+            {
+                [SelectorsKey] = new List<Expression>(),
+                [OrderKey] = new List<Expression>(),
+            };
+        }
+
+        public IQueryBuilderSelect<TTable> Select<TPropertyType>(Expression<Func<TTable, TPropertyType>> selector)
+        {
+            var list = _buildContext[SelectorsKey] as List<Expression>;
+
+            list.Add(selector);
+
+            return this;
+        }
+
+        public IQueryBuilderBuild OrderBy<TPropertyType>(Expression<Func<TTable, TPropertyType>> selector)
+        {
+            var list = _buildContext[OrderKey] as List<Expression>;
+
+            list.Add(selector);
+
+            return this;
+        }
+
+        public string Build(bool isShouldPrint)
+        {
+            var stringBuilder = new StringBuilder();
+
+            ProcessSelectors();
+
+            ProcessFrom();
+
+            ProcessOrder();
+
+            var query = stringBuilder.ToString();
+
+            if (isShouldPrint)
+                Console.WriteLine(query);
+
+            return query;
+
+            void ProcessSelectors()
+            {
+                var expressions = _buildContext[SelectorsKey] as IReadOnlyCollection<Expression>;
+                
+                var columnNames = !expressions.Any() 
+                    ? "* " 
+                    : string.Join(", ", expressions.Select(GetPropertyName));
+
+                stringBuilder
+                    .Append("Select ")
+                    .AppendLine(columnNames);
+            }
+
+            void ProcessFrom()
+            {
+                var type = _buildContext[TableKey] as Type;
+
+                stringBuilder
+                    .Append("From ")
+                    .AppendLine(type.Name);
+            }
+
+            void ProcessOrder()
+            {
+                var expressions = _buildContext[OrderKey] as IReadOnlyCollection<Expression>;
+
+                if (expressions.Any())
+                {
+                    var columnNames = string.Join(", ", expressions.Select(GetPropertyName));
+
+                    stringBuilder
+                        .Append("Order by ")
+                        .Append(columnNames);
+                }
+            }
+        }
+
+        public IQueryBuilderSelect<TTable> From<TTable>()
+        {
+            _buildContext[TableKey] = typeof(TTable);
+            return (IQueryBuilderSelect<TTable>) this;
+        }
+
+        private string GetPropertyName(Expression expression)
+        {
+            var lambdaExpression = expression as LambdaExpression;
+
+            var memberExpression = lambdaExpression.Body as MemberExpression;
+
+            return memberExpression.Member.Name;
+        }
+
+        void Test()
+        {
+            IQueryBuilder builder;
+
+            //builder
+            //    .From<Persons>()
+            //    .Select(x => x.Id)
+            //    .Select(x => x.LastName)
+            //    .OrderBy(x => x.BirthDate)
+            //    .Build();
+        }
+    }
+
+    public class Persons
+    {        
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+
+    public interface IQueryBuilder
+    {
+        IQueryBuilderSelect<TTable> From<TTable>();
+    }
+    
+    public interface IQueryBuilderSelect<TTable> : IQueryBuilderOrder<TTable>, IQueryBuilderBuild
+    {
+        IQueryBuilderSelect<TTable> Select<TPropertyType>(Expression<Func<TTable, TPropertyType>> selector);
+    }
+
+    public interface IQueryBuilderOrder<TTable> : IQueryBuilderBuild
+    {
+        IQueryBuilderBuild OrderBy<TPropertyType>(Expression<Func<TTable, TPropertyType>> selector);
+    }
+
+    public interface IQueryBuilderBuild
+    {
+        string Build(bool isShouldPrint);
+    }
+
+
+}
